30533cd60e557c445a335169f357552e7e3170cb
Pengalu <72057664+Pengalu@users.noreply.github.com>
2023-04-05 13:58:21 -0700

    ai init

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import os; 
 import openai;
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+hi = openai.ChatCompletion.create(
+
+    model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2022?"},
+
+    ]
+    
+)
+print(hi);
